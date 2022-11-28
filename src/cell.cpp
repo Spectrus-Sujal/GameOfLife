1733fcf736314177ec54bda0ab10e8ae0bd653e6
Sujal Naroia <sujalnaroia0099@gmail.com>
2022-11-27 18:45:32 -0600

    Implemented drawCell() and updateState() in cell.cpp

--- src/cell.cpp
+++ src/cell.cpp
@@ -5,12 +5,33 @@ Cell::Cell(state s, Point i) : current{ s }, info{i} {}
 
 void Cell::updateState(int neighbours)
 {
-	
+	// Change state based on neighbours
+	switch(neighbours)
+	{
+		case 2:
+		case 3:
+		// Check if the Cell is coming alive
+			if(neighbours == 3 && current == Cell::state::dead)
+			{
+				current = Cell::state::alive;
+			}
+			break;
+		default:
+			// Make the cell Die
+			if(current == Cell::state::alive)
+			{
+				current = Cell::state::dead;
+			}
+			break;
+	}
 }
 
 void Cell::drawCell() const
 {
-
+	// Change color based on current state
+	if(current == Cell::state::alive) ofSetColor(0);
+	else ofSetColor(255);
+	ofDrawRectangle(info.x, info.y, info.size, info.size);
 }
 
 
