7f532f2ac600129a3d12596546c82c104b2bdd81
Sujal Naroia <snaroia@academic.rrc.ca>
2022-11-30 14:16:14 -0600

    Made the window size absolute and non maximzeable

--- src/main.cpp
+++ src/main.cpp
@@ -3,11 +3,15 @@
 
 //========================================================================
 int main( ){
-	ofSetupOpenGL(1024,768,OF_WINDOW);			// <-------- setup the GL context
 
+	// ofSetupOpenGL(1024,768,OF_WINDOW);			// <-------- setup the GL context
+	ofGLFWWindowSettings settings;
+	settings.resizable = false;
+	ofCreateWindow(settings);
+	return ofRunApp(new ofApp);
 	// this kicks off the running of my app
 	// can be OF_WINDOW or OF_FULLSCREEN
 	// pass in width and height too:
-	ofRunApp(new ofApp());
+	// ofRunApp(new ofApp());
 
 }
